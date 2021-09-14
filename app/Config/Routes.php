bebb919b20a4475dfd7fef8d9fa1e56b93f61a7c
Fajar Magsyar <84609241+fajarmagsyar@users.noreply.github.com>
2021-09-14 22:34:52 +0800

    Vendor and stuff

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,9 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'UserController::index');
 
+//admin
+$routes->get('/goradm', 'AdminController::login');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
